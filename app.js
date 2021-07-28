d9a4f9379572168a376cbc1ab79487a4f79b56b6
Ian B <>
2021-07-28 13:00:51 -0700

    feat: handle attack

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 new Vue({
   el: '#app',
   data: {
-    gameStart: false
+    gameStart: false,
+    you: 100,
+    monster: 100
   },
   methods: {
     handleNewGameClick() {
       this.gameStart = true
+    },
+    handleAttack() {
+      const yourAttack = Math.floor(Math.random() * 10) + 1
+      const monsterAttack = Math.floor(Math.random() * 10) + 1
+
+      this.monster -= yourAttack
+      this.you -= monsterAttack
     }
   }
 })
