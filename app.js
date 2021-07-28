62bb830a88acbc72c557293ac7c48572caac863c
Ian B <>
2021-07-28 13:10:48 -0700

    feat: trigger monster attack after player attack

--- app.js
+++ app.js
@@ -5,16 +5,23 @@ new Vue({
     you: 100,
     monster: 100
   },
+  watch: {
+    monster() {
+      const vm = this
+      
+      setTimeout(function() {
+        const monsterAttack = Math.floor(Math.random() * 10) + 1
+        vm.you -= monsterAttack
+      }, 500)
+    }
+  },
   methods: {
     handleNewGameClick() {
       this.gameStart = true
     },
     handleAttack() {
       const yourAttack = Math.floor(Math.random() * 10) + 1
-      const monsterAttack = Math.floor(Math.random() * 10) + 1
-
       this.monster -= yourAttack
-      this.you -= monsterAttack
     }
   }
 })
